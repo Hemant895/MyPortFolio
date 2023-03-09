12d8b43c44d7f85c36bf2d7c220beaf53cfe0625
HEMANT KHEDKAR <hemantkhedkar102034@gmail.com>
2023-03-09 10:54:28 +0530

    first commit

---
+++ src/app/profile/extra-ciricular/extra-ciricular.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ExtraCiricularComponent } from './extra-ciricular.component';
+
+describe('ExtraCiricularComponent', () => {
+  let component: ExtraCiricularComponent;
+  let fixture: ComponentFixture<ExtraCiricularComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ExtraCiricularComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ExtraCiricularComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
